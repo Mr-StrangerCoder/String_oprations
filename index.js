9468e9d2627689554ec5e367a38c3bc10cca33b8
aadeshsonawane <aadeshsonawane307@gmail.com>
2025-12-29 20:27:05 +0530

    add capitalise function

---
+++ index.js
@@ -0,0 +1,24 @@
+function tocapitalise(){
+
+let paragraph = document.getElementById("floatingTextarea2").value
+
+let toUpCase = paragraph.toUpperCase()
+console.log("hello")
+
+document.getElementById("result").innerHTML = toUpCase
+}
+
+
+// function CountVowel(){
+//     let paragraph = document.getElementById("floatingTextarea2").value
+
+//     for(let i = 0; i <= paragraph.length; i++){
+        
+//     }
+
+
+
+//     document.getElementById("result").innerHTML = 
+// }
+
+
