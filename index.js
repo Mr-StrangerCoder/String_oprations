961a98f157938f706a08c4e33d1954fd832b49a0
aadeshsonawane <aadeshsonawane307@gmail.com>
2025-12-29 21:23:43 +0530

    add countSpaces countConsonests function

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
+
+const str = document.getElementById("floatingTextarea2").value
+
+
 function tocapitalise(){
-
-let paragraph = document.getElementById("floatingTextarea2").value
-
-let toUpCase = paragraph.toUpperCase()
-console.log("hello")
-
+let toUpCase = str.toUpperCase()
 document.getElementById("result").innerHTML = toUpCase
 }
 
 
-// function CountVowel(){
-//     let paragraph = document.getElementById("floatingTextarea2").value
+function CountVowel(){
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" ||  str[i] === "u" || str[i] === "A" || str[i] === "E" || str[i] === "I" || str[i] === "O" ||str[i] === "U")
+            count++
+    }
+    document.getElementById("result").innerHTML = 'total Count of Vowels: ' + count
+}
 
-//     for(let i = 0; i <= paragraph.length; i++){
+
+function countConsonests(){
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" ||  str[i] === "u" || str[i] === "A" || str[i] === "E" || str[i] === "I" || str[i] === "O" ||str[i] === "U"){
+            count++
+        }
         
+    }
+    let countCon = str.length - count
+    document.getElementById("result").innerHTML = 'total Count of consonests: ' + countCon
+}
+
+function tolocase(){
+let toSmCase = str.toLowerCase()
+document.getElementById("result").innerHTML = toSmCase
+}
+
+function countSpaces(){
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str[i] == " ")
+            count++    
+    }
+    document.getElementById("result").innerHTML = 'total spaces: ' + count
+}
+
+// function countNewline(){
+//     let count = 0;
+//     for(let i = 0; i < str.length; i++){
+//         if(str[i] == "/n")
+//             count++    
 //     }
-
-
-
-//     document.getElementById("result").innerHTML = 
+//     document.getElementById("result").innerHTML = 'total New lines: ' + count
 // }
 
 
+function replaceSpaces(){
+    
+}
